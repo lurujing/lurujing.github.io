b2c694542544d92e99c9cbd05be6273d27661037
lurujing <1483857549@qq.com>
2017-12-05 10:49:37 +0800

    删除了

--- 二期答辩/js/zhuye.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    var c=0;
-    function run(){
-        c++;
-        c = (c == 3) ? 0 : c;
-        $("#pic img").eq(c).fadeIn(1000).siblings("img").fadeOut(1000);
-    }
-    var timer=setInterval(run,3000)
-})
