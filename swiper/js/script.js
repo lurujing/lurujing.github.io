69110d2a343fc1a0399b10700d0903099850c921
lurujing <1483857549@qq.com>
2017-12-05 09:31:48 +0800

    添加了swiper

---
+++ swiper/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 17/12/5 005.
+ */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next'
+    }
+});
