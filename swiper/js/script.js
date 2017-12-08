3ee4cd5be221b8794de63252505cc4e1e1e13037
lurujing <1483857549@qq.com>
2017-12-08 11:19:35 +0800

    添加了animate

--- swiper/js/script.js
+++ swiper/js/script.js
@@ -4,12 +4,24 @@
 var swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     pagination: {
-    el: '.swiper-pagination',
+        el: '.swiper-pagination',
         clickable: true,
-},
-// 如果需要前进后退按钮
-navigation: {
-    nextEl: '.swiper-button-next'
-},
-    loop:true,
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next'
+    },
+    loop: true,
+
+//animate动画
+    on: {
+        init: function () {
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function () {
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+
 });
