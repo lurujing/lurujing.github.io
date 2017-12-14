d539d7e4ac90a59eb703f0041443ee53924bf588
lurujing <1483857549@qq.com>
2017-12-14 17:20:05 +0800

    tianjia

--- shuangxuan/js/script.js
+++ shuangxuan/js/script.js
@@ -8,4 +8,31 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
     },
+
+
+    //animate动画
+    on: {
+        init: function () {
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function () {
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+
 });
+
+
+//音乐小图标
+var mp3=document.querySelector("#mp3");
+var mp3Btn=document.querySelector("#mp3Btn");
+mp3Btn.onclick=function(){
+    if(mp3.paused){
+        mp3Btn.style.animationPlayState='running';
+        mp3.play();
+    }else{
+        mp3Btn.style.animationPlayState='paused';
+        mp3.pause();
+    }
+}
